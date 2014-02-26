3961068d4064f2498a582635ff449536b3b7c64f
jmat <jmat@alum.mit.edu>
2014-02-25 22:43:57 -0500

    Kewl. Now it works. Not portable, but not too shabby.

--- Base64EncodeImages/src/Main.hx
+++ Base64EncodeImages/src/Main.hx
@@ -1,6 +1,7 @@
 package ;
 
-import haxe.Serializer;
+import haxe.io.Bytes;
+import neko.Lib;
 import sys.FileSystem;
 import sys.io.File;
 
@@ -10,13 +11,19 @@ class Main {
 		var images = FileSystem.readDirectory(imageDir);
 		var imagePaths = images.map(function(image) return imageDir + "/" + image);
 		var imageBytes = imagePaths.map(File.getBytes);
-		var imageBase64s = imageBytes.map(Serializer.run);
-		var imageHTMLs = imageBase64s.map(function(imageBase64) return "\r\n<p><img src='data:image/jpg;base64," + imageBase64 + "'/></p>");
+		var imageBase64s = imageBytes.map(ConvertToBase64String);
+		var imageHTMLs = imageBase64s.map(function(imageBase64) return "<p><img src='data:image/jpg;base64," + imageBase64 + "'/></p>");
 
 		var page = new StringBuf();
 		page.add("<html><body>");
-		for(imageHTML in imageHTMLs) page.add(imageHTML);
+		for (imageHTML in imageHTMLs) page.add(imageHTML);
 		page.add("</body></html>");
 		File.saveContent("Images.html", page.toString());
+	}
+
+	static var NekoStdBaseEncode = Lib.load("std", "base_encode", 2);
+	static var BASE64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+	static function ConvertToBase64String(v : Bytes) {
+		return new String(NekoStdBaseEncode(v.getData(), untyped BASE64.__s));
 	}
 }
