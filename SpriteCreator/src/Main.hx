932990e2f82f17c90324b755301cd1a35193b9a3
jmat <jmat@alum.mit.edu>
2014-02-26 22:10:45 -0500

    Built sprite from the images with the max width.

--- SpriteCreator/src/Main.hx
+++ SpriteCreator/src/Main.hx
@@ -1,9 +1,11 @@
 package ;
 
+import haxe.io.BytesOutput;
 import sys.FileSystem;
 import sys.io.File;
 import format.png.Reader;
 import format.png.Tools;
+import format.png.Writer;
 
 class Main {
 	
@@ -14,14 +16,28 @@ class Main {
 		var imageDatas = imagePaths.map(function(imagePath) return new Reader(File.read(imagePath, true)).read());
 		var maxWidthTotalHeight = Lambda.fold(imageDatas, function(imageData, maxWidthTotalHeight) {
 			var header = Tools.getHeader(imageData);
-			maxWidthTotalHeight[0] = Math.max(maxWidthTotalHeight[0], header.width);
-			maxWidthTotalHeight[1] += header.height;
+			maxWidthTotalHeight.MaxWidth = Math.max(maxWidthTotalHeight.MaxWidth, header.width);
+			maxWidthTotalHeight.TotalHeight += header.height;
 			return maxWidthTotalHeight;
-		}, [0, 0]);
+		}, {MaxWidth : 0, TotalHeight : 0});
 		for (imageData in imageDatas) {
 			var header = Tools.getHeader(imageData);
-			Sys.println(header.width + ", " + header.height);
+			Sys.println(header.width + ", " + header.height + ", " + header.colbits + ", " + header.color);
 		}
-			Sys.println(maxWidthTotalHeight);
+		Sys.println(maxWidthTotalHeight);
+		
+		var spriteBytes = new BytesOutput();
+		for (imageData in imageDatas) {
+			var header = Tools.getHeader(imageData);
+			if (header.width < maxWidthTotalHeight.MaxWidth) {
+				maxWidthTotalHeight.TotalHeight -= header.height;
+				continue;
+			}
+			var imageDataBytes = Tools.extract32(imageData);
+			spriteBytes.writeBytes(imageDataBytes, 0, imageDataBytes.length);
+		}
+
+		var spriteData = Tools.build32BGRA(Std.int(maxWidthTotalHeight.MaxWidth), maxWidthTotalHeight.TotalHeight, spriteBytes.getBytes());
+		new Writer(File.write("sprite.png", true)).write(spriteData);
 	}
 }
