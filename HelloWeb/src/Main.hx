3e38416cfc8d6a6db261ec1e17cd576e75c37dc8
jmat <jmat@alum.mit.edu>
2014-02-28 22:08:25 -0500

    Cleaned up the output.
    Added more comments.

--- HelloWeb/src/Main.hx
+++ HelloWeb/src/Main.hx
@@ -1,14 +1,18 @@
 // With the output file being "HelloWeb.n" the url is "http://localhost:2000/HelloWeb.n"
 // Parameters work as normal ("http://localhost:2000/HelloWeb.n?foo=bar&baz=biff").
+// Also, the server doesn't need to stopped and restarted to pick up new changes.
+
 package ;
+
 import neko.Web;
 
 class Main {
 	static function main() {
-		trace("<pre>");
-		trace("Hello (trace) World!");
+		trace("Hello World!");
+		Sys.println("<br/>");
 		trace(Web.getURI());
+		Sys.println("<br/>");
 		trace(Web.getParams());
-		trace("</pre>");
+		Sys.println("<br/>");
 	}
 }
