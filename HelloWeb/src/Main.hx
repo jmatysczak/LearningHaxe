9bd9f48cdd7115444114bd5556ddbeb57e0e876a
jmat <jmat@alum.mit.edu>
2014-02-28 21:52:46 -0500

    Can't figure out standard in...not sure I just want to call it...so I'll rely on the neko api.

--- HelloWeb/src/Main.hx
+++ HelloWeb/src/Main.hx
@@ -1,10 +1,14 @@
 // With the output file being "HelloWeb.n" the url is "http://localhost:2000/HelloWeb.n"
 // Parameters work as normal ("http://localhost:2000/HelloWeb.n?foo=bar&baz=biff").
 package ;
+import neko.Web;
 
 class Main {
 	static function main() {
+		trace("<pre>");
 		trace("Hello (trace) World!");
-		Sys.println("Hello (Sys.println) World!");
+		trace(Web.getURI());
+		trace(Web.getParams());
+		trace("</pre>");
 	}
 }
