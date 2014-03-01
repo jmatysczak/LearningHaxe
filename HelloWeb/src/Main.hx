ee0962a805d81feed1534adc7455e965189deec1
jmat <jmat@alum.mit.edu>
2014-02-28 21:29:04 -0500

    Added "note to self" about the url.

--- HelloWeb/src/Main.hx
+++ HelloWeb/src/Main.hx
@@ -1,3 +1,5 @@
+// With the output file being "HelloWeb.n" the url is "http://localhost:2000/HelloWeb.n"
+// Parameters work as normal ("http://localhost:2000/HelloWeb.n?foo=bar&baz=biff").
 package ;
 
 class Main {
