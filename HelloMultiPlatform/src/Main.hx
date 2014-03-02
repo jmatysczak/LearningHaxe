1b92b8fc97ed59ac0a2a3ac7c7dfe6cc52409d65
jmat <jmat@alum.mit.edu>
2014-03-02 15:32:05 -0500

    I got it now.
    I screwed up thinking the macro was executed at compilation time.

--- HelloMultiPlatform/src/Main.hx
+++ HelloMultiPlatform/src/Main.hx
@@ -1,7 +1,7 @@
 package ;
 
 class Main {
-	macro static function getCompilationTarget() {
+	macro static function getCompilationTargetMacro() {
 		#if cs
 		return macro "c#";
 		#elseif java
@@ -12,7 +12,18 @@ class Main {
 		return macro "not supported by 'getCompilationTarget'";
 		#end
 	}
+	static function getCompilationTarget() {
+		#if cs
+		return "c#";
+		#elseif java
+		return "java";
+		#elseif neko
+		return "neko";
+		#else
+		return "not supported by 'getCompilationTarget'";
+		#end
+	}
 	static function main() {
-		Sys.println('Hello (${getCompilationTarget()}) World: ${Sys.executablePath()}');
+		Sys.println('Hello (${getCompilationTarget()}) World');
 	}
 }
