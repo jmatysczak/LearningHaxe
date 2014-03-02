0cf8803c1ea8c20ca7e634a57ba2de75f7030062
jmat <jmat@alum.mit.edu>
2014-03-02 15:40:54 -0500

    Don't need this.

--- HelloMultiPlatform/src/Main.hx
+++ HelloMultiPlatform/src/Main.hx
@@ -1,17 +1,7 @@
 package ;
+import haxe.macro.Context;
 
 class Main {
-	macro static function getCompilationTargetMacro() {
-		#if cs
-		return macro "c#";
-		#elseif java
-		return macro "java";
-		#elseif neko
-		return macro "neko";
-		#else
-		return macro "not supported by 'getCompilationTarget'";
-		#end
-	}
 	static function getCompilationTarget() {
 		#if cs
 		return "c#";
