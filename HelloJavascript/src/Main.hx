fecb8d95bf023e6831ef97299ab2e70c78dd3e7c
jmat <jmat@alum.mit.edu>
2014-03-02 14:08:41 -0500

    A javascript project.

---
+++ HelloJavascript/src/Main.hx
@@ -0,0 +1,9 @@
+package ;
+
+class Main {
+	static function main() {
+		// The below commented out line doesn't compile with error: Accessing this field requires a system platform (php,neko,cpp,etc.)
+		//Sys.println("Hello World");
+		trace("Hello World");
+	}
+}
