bb84a0e576b72df8e782d22a50388a7b5b8a9761
jmat <jmat@alum.mit.edu>
2014-03-08 16:20:40 -0500

    Can't figure out how to not run the C# and Java compilation in the post build.
    So I moved it to the batch file and commented it out.
    Just too slow...

--- Knapsack/Run.bat
+++ Knapsack/Run.bat
@@ -1,7 +1,13 @@
 @echo off
+rem haxe -cp src -cp test -cs $(OutputDir)\$(ProjectName)-CS -main Main -$(BuildConfig)
+rem haxe -cp src -cp test -java $(OutputDir)\$(ProjectName)-Java -main Main -$(BuildConfig)
+
 copy test\knapsack\*.txt bin
+
 cd bin
+
 neko Knapsack.n
 rem Knapsack-CS\bin\Knapsack-CS.exe
 rem java -jar Knapsack-Java\Knapsack-Java.jar
+
 pause
