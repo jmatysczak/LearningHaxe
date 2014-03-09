3d584a57a5a1f6ce84becbdc2244371c0dc1d4bc
jmat <jmat@alum.mit.edu>
2014-03-08 20:19:17 -0500

    Minor code cleanup for determining if a bit is set.

--- Knapsack/src/knapsack/FullSearch.hx
+++ Knapsack/src/knapsack/FullSearch.hx
@@ -1,6 +1,8 @@
 package knapsack;
 import haxe.ds.Vector.Vector;
 
+using knapsack.FullSearch;
+
 class FullSearch {
 	public static function Find(valuables: Array<Valuable>, weightLimit: Float) {
 		if (valuables.length > 30) throw "FullSearch.Find does not support more than 30 Valuable items.";
@@ -13,12 +15,11 @@ class FullSearch {
 		solution.Valuables = valuables;
 		solution.WeightLimit = weightLimit;
 
-		Sys.println(maxSolution);
 		for(inSolution in 1...maxSolution) {
 			var value: Float = 0,
 				weight: Float = 0;
 			for (i in 0...valuables.length) {
-				if ((inSolution & (1 << i)) != 0) {
+				if (inSolution.hasBitSet(i)) {
 					value += valuables[i].Value;
 					weight += valuables[i].Weight;
 				}
@@ -31,9 +32,13 @@ class FullSearch {
 		}
 
 		var ids = [];
-		for (i in 0...valuables.length) if ((bestInSolution & (1 << i)) != 0) ids.push(valuables[i].Id);
+		for (i in 0...valuables.length) if (bestInSolution.hasBitSet(i)) ids.push(valuables[i].Id);
 		solution.Best = new Valuables(ids, bestValue, bestWeight);
 
 		return solution;
+	}
+
+	inline static function hasBitSet(n:Int, i: Int) {
+		return (n & (1 << i)) != 0;
 	}
 }
