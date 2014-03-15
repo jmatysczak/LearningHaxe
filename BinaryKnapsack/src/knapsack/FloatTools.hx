5de2790e6816070048ac1b123be31988c74032a7
jmat <jmat@alum.mit.edu>
2014-03-14 21:09:46 -0400

    Renamed the default return value since it has to be used for all values.

--- BinaryKnapsack/src/knapsack/FloatTools.hx
+++ BinaryKnapsack/src/knapsack/FloatTools.hx
@@ -1,10 +1,10 @@
 package knapsack;
 
 class FloatTools {
-	public inline static function compareTo(me: Float, other: Float, equalsValue = 0): Int {
+	public inline static function compareTo(me: Float, other: Float, returnValue = 0): Int {
 			var diff = me - other;
-			if (diff < 0) equalsValue = -1;
-			if (diff > 0) equalsValue = 1;
-			return equalsValue;
+			if (diff < 0) returnValue = -1;
+			if (diff > 0) returnValue = 1;
+			return returnValue;
 	}
 }
