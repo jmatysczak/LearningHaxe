f2f88427c04ef20887cc387bf1f9be03746bd569
jmat <jmat@alum.mit.edu>
2014-03-16 14:31:48 -0400

    Added C# and Java compilation and running.

--- BinaryKnapsack/Run.bat
+++ BinaryKnapsack/Run.bat
@@ -1,6 +1,4 @@
 @echo off
-rem haxe -cp src -cp test -cs $(OutputDir)\$(ProjectName)-CS -main Main -$(BuildConfig)
-rem haxe -cp src -cp test -java $(OutputDir)\$(ProjectName)-Java -main Main -$(BuildConfig)
 
 del bin\error_*
 del bin\example_*
@@ -9,8 +7,8 @@ copy test\knapsack\example_* bin
 cd bin
 
 neko BinaryKnapsack.n
-rem BinaryKnapsack-CS\bin\BinaryKnapsack-CS.exe
-rem java -jar BinaryKnapsack-Java\BinaryKnapsack-Java.jar
-rem "c:\Program Files (x86)\Mono-2.10.9\bin\mono.exe" BinaryKnapsack-CS\bin\BinaryKnapsack-CS.exe
+BinaryKnapsack-CS\bin\BinaryKnapsack-CS.exe
+java -jar BinaryKnapsack-Java\BinaryKnapsack-Java.jar
+"c:\Program Files (x86)\Mono-2.10.9\bin\mono.exe" BinaryKnapsack-CS\bin\BinaryKnapsack-CS.exe
 
 pause
