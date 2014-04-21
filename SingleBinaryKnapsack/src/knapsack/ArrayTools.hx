3a2fd1aa5cd82ba031a94fe2b9d361a21ee15e5e
jmat <jmat@alum.mit.edu>
2014-04-20 21:03:59 -0400

    Doesn't need to be inline.

--- SingleBinaryKnapsack/src/knapsack/ArrayTools.hx
+++ SingleBinaryKnapsack/src/knapsack/ArrayTools.hx
@@ -3,7 +3,7 @@ package knapsack;
 import haxe.ds.Vector.Vector;
 
 class ArrayTools {
-	public inline static function mapi<A, B>(a: Array<A>, f: Int -> A -> B) : Array<B> {
+	public static function mapi<A, B>(a: Array<A>, f: Int -> A -> B) : Array<B> {
 		var r = new Vector<B>(a.length);
 		for (i in 0...a.length) r[i] = f(i, a[i]);
 
