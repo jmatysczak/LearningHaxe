fe0bdb2c166ce04825eade63947dee41fa24ce76
jmat <jmat@alum.mit.edu>
2014-04-20 21:07:58 -0400

    Refactor the code to make it a little cleaner.

--- SingleBinaryKnapsack/src/knapsack/ArrayTools.hx
+++ SingleBinaryKnapsack/src/knapsack/ArrayTools.hx
@@ -3,13 +3,16 @@ package knapsack;
 import haxe.ds.Vector.Vector;
 
 class ArrayTools {
-	public static function mapi<A, B>(a: Array<A>, f: Int -> A -> B) : Array<B> {
-		var r = new Vector<B>(a.length);
-		for (i in 0...a.length) r[i] = f(i, a[i]);
-
-		var a = new Array();
-		Reflect.setField(a, "__a", r);
-		Reflect.setProperty(a, "length", r.length);
+	public static function fromVector<A>(v: Vector<A>): Array<A> {
+		var a = new Array<A>();
+		Reflect.setField(a, "__a", v);
+		Reflect.setProperty(a, "length", v.length);
 		return a;
+	}
+
+	public static function mapi<A, B>(a: Array<A>, f: Int -> A -> B) : Array<B> {
+		var v = new Vector<B>(a.length);
+		for (i in 0...a.length) v[i] = f(i, a[i]);
+		return ArrayTools.fromVector(v);
 	}
 }
